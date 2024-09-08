8e7c49e3d2a0a3d0a6ae3cfd14942e45acbbce24
ettabet aicha <aicha70tabite@gmail.com>
2024-09-08 21:39:11 +0100

    declare proprty and construct for each class with getters and setters

--- src/com/example/greenplusdb/model/Consommation.java
+++ src/com/example/greenplusdb/model/Consommation.java
@@ -1,4 +1,67 @@
 package com.example.greenplusdb.model;
+import java.sql.Timestamp;
+public abstract class Consommation{
+    private int id;
+    private User user;
+    private TypeConsommation typeConsommation;
+    private  double impact;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
 
-public class Consommation {
+    public Consommation(User user, TypeConsommation typeConsommation, double impact) {
+        this.user = user;
+        this.typeConsommation = typeConsommation;
+        this.impact = impact;
+        this.createdAt = new Timestamp(System.currentTimeMillis());
+        this.updatedAt = new Timestamp(System.currentTimeMillis());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public TypeConsommation getTypeConsommation() {
+        return typeConsommation;
+    }
+
+    public void setTypeConsommation(TypeConsommation typeConsommation) {
+        this.typeConsommation = typeConsommation;
+    }
+
+    public double getImpact() {
+        return impact;
+    }
+
+    public void setImpact(double impact) {
+        this.impact = impact;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    public abstract double calculerImpact();
 }
