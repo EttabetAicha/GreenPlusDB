b8b713281d725d7da5ee6b76fa8e8f14b8998e47
ettabet aicha <aicha70tabite@gmail.com>
2024-09-09 12:16:19 +0100

    declare proprty and construct for each class with getters and setters

--- src/com/example/greenplusdb/model/Alimentation.java
+++ src/com/example/greenplusdb/model/Alimentation.java
@@ -1,5 +1,49 @@
 package com.example.greenplusdb.model;
 
-public class Alimentation {
+public class Alimentation extends Consommation {
+    private TypeAliment typeAliment;
+    private double poids;
 
+
+    public Alimentation(User user, TypeAliment typeAliment, double poids) {
+
+        super(user, TypeConsommation.ALIMENTATION, calculateAlimentationImpact(poids, typeAliment));
+        this.typeAliment = typeAliment;
+        this.poids = poids;
+    }
+
+
+    public TypeAliment getTypeAliment() {
+        return typeAliment;
+    }
+
+    public void setTypeAliment(TypeAliment typeAliment) {
+        this.typeAliment = typeAliment;
+    }
+
+    public double getPoids() {
+        return poids;
+    }
+
+    public void setPoids(double poids) {
+        this.poids = poids;
+    }
+
+
+    @Override
+    public double calculerImpact() {
+        return calculateAlimentationImpact(poids, typeAliment);
+    }
+
+
+    private static double calculateAlimentationImpact(double poids, TypeAliment typeAliment) {
+        switch (typeAliment) {
+            case VIANDE:
+                return poids * 5.0;
+            case LEGUME:
+                return poids * 0.5;
+            default:
+                throw new IllegalArgumentException("Unknown food type: " + typeAliment);
+        }
+    }
 }
--- src/com/example/greenplusdb/model/Logement.java
+++ src/com/example/greenplusdb/model/Logement.java
@@ -1,4 +1,49 @@
 package com.example.greenplusdb.model;
 
-public class Logement {
+public class Logement extends Consommation {
+    private TypeEnergie typeEnergie;
+    private double consommationEnergie;
+
+
+    public Logement(User user, TypeEnergie typeEnergie, double consommationEnergie) {
+        // Calculate the impact before calling super()
+        super(user, TypeConsommation.LOGEMENT, calculateLogementImpact(consommationEnergie, typeEnergie));
+        this.typeEnergie = typeEnergie;
+        this.consommationEnergie = consommationEnergie;
+    }
+
+
+    public TypeEnergie getTypeEnergie() {
+        return typeEnergie;
+    }
+
+    public void setTypeEnergie(TypeEnergie typeEnergie) {
+        this.typeEnergie = typeEnergie;
+    }
+
+    public double getConsommationEnergie() {
+        return consommationEnergie;
+    }
+
+    public void setConsommationEnergie(double consommationEnergie) {
+        this.consommationEnergie = consommationEnergie;
+    }
+
+
+    @Override
+    public double calculerImpact() {
+        return calculateLogementImpact(consommationEnergie, typeEnergie);
+    }
+
+
+    private static double calculateLogementImpact(double consommationEnergie, TypeEnergie typeEnergie) {
+        switch (typeEnergie) {
+            case ELECTRICITE:
+                return consommationEnergie * 1.5;
+            case GAZ:
+                return consommationEnergie * 2.0;
+            default:
+                throw new IllegalArgumentException("Unknown energy type: " + typeEnergie);
+        }
+    }
 }
